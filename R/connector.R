757ff89e19a987f91a2a85997609761ac174ffeb
Aksel Thomsen <oath@novonordisk.com>
2024-05-07 15:30:54 +0200

    feat: playing around with how to initialise the package

---
+++ R/connector.R
@@ -0,0 +1,40 @@
+#' Connect to a database
+#' @param yaml A yaml file with the connection information
+#' @param ... Arguments to pass to the connector
+#' @return A new connector object
+#' @export
+
+connect <- function(yaml = NULL, ...) {
+
+  args <- rlang::list2(...)
+
+  if (!is.null(yaml)) {
+    args <- yaml::read_yaml(yaml) |>
+      c(args)
+  }
+
+  do.call(connector$new, args)
+}
+
+#' R6 general connector class
+#' @export
+
+connector <- R6::R6Class("connector",
+  public = list(
+    connection = NULL,
+    initialize = function() {
+      self$connection <- NULL
+    },
+    connect = function() {
+      self$connection <- dbConnect(RSQLite::SQLite(), dbname = "database.sqlite")
+    },
+    disconnect = function() {
+      dbDisconnect(self$connection)
+    }
+  ),
+  private = list(
+    finalize = function() "close connections"
+  )
+)
+
+
