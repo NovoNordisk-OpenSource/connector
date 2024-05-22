0058eecff386a6888a61f4fb184c6a7a1bad27e2
Aksel Thomsen <oath@novonordisk.com>
2024-05-22 15:48:17 +0200

    feat: intitial dbi connector class

---
+++ tests/testthat/test-dbi.R
@@ -0,0 +1,37 @@
+test_that("SQLite", {
+
+  test <- connector_dbi$new(drv = RSQLite::SQLite(), dbname = withr::local_tempfile())
+
+  x <- mtcars
+  x$car <- rownames(x)
+  rownames(x) <- NULL
+
+  test$list_content() |>
+    expect_equal(character(0))
+
+  test$write(x, "mtcars") |>
+    expect_true()
+
+  test$write(x, "mtcars") |>
+    expect_error()
+
+  test$list_content() |>
+    expect_equal("mtcars")
+
+  test$read("mtcars") |>
+    expect_equal(x)
+
+  test$write(x, "mtcars", overwrite = TRUE) |>
+    expect_true()
+
+  test$tbl("mtcars") |>
+    dplyr::filter(car == "Mazda RX4") |>
+    dplyr::select(car, mpg) |>
+    dplyr::collect() |>
+    expect_equal(dplyr::tibble(car = "Mazda RX4", mpg = 21))
+
+  test$get_conn() |>
+    DBI::dbGetQuery("SELECT * FROM mtcars") |>
+    expect_equal(x)
+
+})
