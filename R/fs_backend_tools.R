b723a9728e03092a0ed4526ed3f9fba87f3dac1c
CGID (Cervan Girard) <CGID@novonordisk.com>
2024-06-04 13:12:55 +0200

    feat: implement units tests and read yaml fct

--- R/fs_backend_tools.R
+++ R/fs_backend_tools.R
@@ -18,8 +18,9 @@ get_backend_fct <- function(backend_type) {
     )
   } else {
     package_name <- gsub("\\:{2,3}[^\\:]+$", backend_type, replacement = "")
+    function_name <- gsub("^.+\\:{2,3}|\\(\\)", "", backend_type)
 
-    return(getExportedValue(package_name, backend_type))
+    return(getExportedValue(package_name, function_name))
   }
 }
 
@@ -54,7 +55,7 @@ extract_info_from_metadata <- function(extracted_metadata, path) {
     str_detect("^metadata\\."))
 
   if (!be_sure_metadata) {
-    stop("Only metadata is allowed for now in the custom path")
+    stop("Only metadata is allowed for now in custom variables.")
   }
 
   ## Extract metadata
@@ -95,9 +96,13 @@ extract_custom_path <- function(yaml_content, path) {
 #'
 #' @param yaml_content The yaml content
 #' @param backend The backend to create and because of file system, a "path" is mandatory
+#' @param name The name of the connection
+#'
 #'
 #' @return A new backend based on R6 class
 #' @export
+#'
+#' @importFrom purrr set_names
 #'
 #' @examples
 #' yaml_file <- system.file("config", "default_config.yml", package = "connector")
@@ -106,23 +111,31 @@ extract_custom_path <- function(yaml_content, path) {
 #' ## Extract fct
 #' my_backend <- only_one %>%
 #'   extract_backends()
-#' # Create the backend for fs
-#' test <- create_backend_fs(yaml_content, my_backend)
-#'
-create_backend_fs <- function(yaml_content, backend) {
+#' name <- only_one %>%
+#'   extract_con()
+#' # Create the backend
+#' test <- create_backend_fs(yaml_content = yaml_content, backend = my_backend, name = name)
+create_backend_fs <- function(yaml_content, backend, name) {
   params_from_user <- backend[names(backend) != c("type")]
 
   if (!("path" %in% names(backend))) {
     stop("Path is mandatory for connector_fs")
   }
 
-  path <- backend$path
-
-  custom_path <- extract_custom_path(yaml_content, path)
-
-  params_from_user$path <- custom_path
+  # extract metadata if needed
+  params_from_user <- purrr::map(params_from_user, function(x) {
+    extract_custom_path(yaml_content, x)
+  }) %>%
+    purrr::set_names(names(params_from_user))
 
   connect_fct <- get_backend_fct(backend$type)
 
-  do.call(connect_fct, params_from_user)
+  connect_ <- do.call(connect_fct, params_from_user)
+
+  return(
+    list(
+      backend = connect_
+    ) %>%
+      purrr::set_names(name)
+  )
 }
