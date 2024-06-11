88f504dcf7f36e57f6777ee47ed82f9975f326e2
Aksel Thomsen (OATH) <oath@novonordisk.com>
2024-06-11 11:12:00 +0200

    wip: added assertations

--- R/read_yaml.R
+++ R/read_yaml.R
@@ -1,6 +1,5 @@
 #' Read and parse yaml configuration file
 #' @param file [character] Path to yaml file
-#' @param eval.expr [logical] Evaluate expressions in yaml file. Passed to [yaml::read_yaml]
 #' @param set_env [logical] Should environment variables from the yaml file be set. Default is TRUE.
 #' @return Configuration [list] with all content evaluated
 #' @examples
@@ -11,14 +10,13 @@
 #' Sys.getenv("hello")
 #' @export
 
-read_yaml_config <- function(file, eval.expr = TRUE, set_env = TRUE) {
+read_yaml_config <- function(file, set_env = TRUE) {
   val <- checkmate::makeAssertCollection()
   checkmate::assert_file_exists(x = file, access = "r", add = val)
-  checkmate::assert_logical(x = eval.expr, add = val)
   checkmate::assert_logical(x = set_env, add = val)
   zephyr::report_checkmate_assertations(collection = val)
 
-  config <- yaml::read_yaml(file = file, eval.expr = eval.expr) |>
+  config <- yaml::read_yaml(file = file, eval.expr = TRUE) |>
     assert_config()
 
   # Parse env variables
@@ -55,35 +53,103 @@ read_yaml_config <- function(file, eval.expr = TRUE, set_env = TRUE) {
   return(config)
 }
 
+#' Input validation:
+#'
+#' - Only metadata, env, connections, and datasources are allowed
+#' - Everything must be named
+#' - connections and datasources are mandatory
+#' - metadata and env must each be a list of named character vectors of length 1
+#' - connections and datasources must each be a list of unnamed lists
+#' - each connection must have the named character element "con" and the named list element "backend"
+#' - each datasource must have the named character element "name"
 #' @noRd
 
-assert_config <- function(config) {
-  # Input validation:
-  #
-  # - Only metadata, env, connections, and datasources are allowed
-  # - Everything must be named
-  # - connections and datasources are mandatory
-  # - metadata and env must each be a list of named character vectors of length 1
-  # - connections and datasources must each be a list of unnamed lists
-  # - each connection must have the named character element "con" and the named list element "backend"
-  # - each datasource must have the named character element "name"
-
-  # TODO: Implement all of the above
+assert_config <- function(config, env = parent.frame()) {
 
   val <- checkmate::makeAssertCollection()
 
   checkmate::assert_list(x = config, names = "unique", add = val)
+
   checkmate::assert_names(
     x = names(config),
     type = "unique",
     subset.of = c("metadata", "env", "connections", "datasources"),
     must.include = c("connections", "datasources"),
-    what = "index",
+    what = "Config",
     .var.name = "yaml",
     add = val
   )
 
-  zephyr::report_checkmate_assertations(collection = val, msg = "Invalid configuration file:")
+  checkmate::assert_list(
+    x = config[["metadata"]],
+    names = "unique",
+    null.ok = TRUE,
+    .var.name = "metadata",
+    add = val
+  )
+
+  purrr::walk2(
+    .x = config[["metadata"]],
+    .y = names(config[["metadata"]]),
+    .f = \(x, y) {
+      checkmate::assert_character(x, len = 1, .var.name = paste0("metadata.",y), add = val)
+    })
+
+  checkmate::assert_list(
+    x = config[["env"]],
+    names = "unique",
+    null.ok = TRUE,
+    .var.name = "env",
+    add = val
+  )
+
+  purrr::walk2(
+    .x = config[["env"]],
+    .y = names(config[["env"]]),
+    .f = \(x, y) {
+      checkmate::assert_character(x, len = 1, .var.name = paste0("env.",y), add = val)
+    })
+
+  checkmate::assert_list(
+    x = config[["connections"]],
+    null.ok = FALSE,
+    .var.name = "connections",
+    add = val
+  )
+
+  purrr::walk2(
+    .x = config[["connections"]],
+    .y = seq_along(config[["connections"]]),
+    .f = \(x, y) {
+      var = paste0("connections.",y)
+      checkmate::assert_list(x, .var.name = var, add = val)
+      checkmate::assert_names(names(x), type = "unique", must.include = c("con", "backend"), .var.name = var, add = val)
+      checkmate::assert_character(x[["con"]], len = 1, .var.name = paste0(var,".con"), add = val)
+      checkmate::assert_list(x[["backend"]], names = "unique", .var.name = paste0(var, ".backend"), add = val)
+    })
+
+  checkmate::assert_list(
+    x = config[["datasources"]],
+    null.ok = FALSE,
+    .var.name = "datasources",
+    add = val
+  )
+
+  purrr::walk2(
+    .x = config[["datasources"]],
+    .y = seq_along(config[["datasources"]]),
+    .f = \(x, y) {
+      var = paste0("datasources.",y)
+      checkmate::assert_list(x, .var.name = var, add = val)
+      checkmate::assert_names(names(x), type = "unique", must.include = c("name"), .var.name = var, add = val)
+      checkmate::assert_character(x[["name"]], len = 1, .var.name = paste0(var,".name"), add = val)
+    })
+
+  zephyr::report_checkmate_assertations(
+    collection = val,
+    msg = "Invalid configuration file:",
+    env = env
+    )
 
   return(invisible(config))
 }
