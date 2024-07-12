c92ff25daa4a1417411f924670ef82587b986689
Aksel Thomsen (OATH) <oath@novonordisk.com>
2024-07-12 14:56:01 +0200

    fix: lintr issues

--- R/dbi_methods.R
+++ R/dbi_methods.R
@@ -14,8 +14,8 @@
 #' connector$read("iris")
 #'
 cnt_read.connector_dbi <- function(connector_object, name, ...) {
-    connector_object$get_conn() %>%
-        DBI::dbReadTable(name = name, ...)
+  connector_object$get_conn() %>%
+    DBI::dbReadTable(name = name, ...)
 }
 
 
@@ -30,8 +30,8 @@ cnt_read.connector_dbi <- function(connector_object, name, ...) {
 #' connector <- connector_dbi$new(RSQLite::SQLite())
 #' connector$write(iris, "iris")
 cnt_write.connector_dbi <- function(connector_object, x, name, ...) {
-    connector_object$get_conn() %>%
-        DBI::dbWriteTable(name = name, value = x, ...)
+  connector_object$get_conn() %>%
+    DBI::dbWriteTable(name = name, value = x, ...)
 }
 
 #' List tables in the database
@@ -44,8 +44,8 @@ cnt_write.connector_dbi <- function(connector_object, x, name, ...) {
 #' connector$write(iris, "iris")
 #' connector$list_content()
 cnt_list_content.connector_dbi <- function(connector_object, ...) {
-    connector_object$get_conn() %>%
-        DBI::dbListTables(...)
+  connector_object$get_conn() %>%
+    DBI::dbListTables(...)
 }
 
 #' Remove a table from the database
@@ -58,8 +58,8 @@ cnt_list_content.connector_dbi <- function(connector_object, ...) {
 #' connector$write(iris, "iris")
 #' connector$remove("iris")
 cnt_remove.connector_dbi <- function(connector_object, name, ...) {
-    connector_object$get_conn() %>%
-        DBI::dbRemoveTable(name = name, ...)
+  connector_object$get_conn() %>%
+    DBI::dbRemoveTable(name = name, ...)
 }
 
 #' Create a [tbl] object
@@ -72,8 +72,8 @@ cnt_remove.connector_dbi <- function(connector_object, name, ...) {
 #' connector$write(iris, "iris")
 #' connector$tbl("iris")
 cnt_tbl.connector_dbi <- function(connector_object, name, ...) {
-    connector_object$get_conn() %>%
-        dplyr::tbl(from = name, ...)
+  connector_object$get_conn() %>%
+    dplyr::tbl(from = name, ...)
 }
 
 #' Disconnect from the database
@@ -84,6 +84,6 @@ cnt_tbl.connector_dbi <- function(connector_object, name, ...) {
 #' connector <- connector_dbi$new(RSQLite::SQLite())
 #' connector$disconnect()
 cnt_disconnect.connector_dbi <- function(connector_object, ...) {
-    connector_object$get_conn() %>%
-        DBI::dbDisconnect(...)
+  connector_object$get_conn() %>%
+    DBI::dbDisconnect(...)
 }
