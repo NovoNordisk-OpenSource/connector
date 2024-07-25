08ccbfaa9f08df7c8705f3a5162aa8adbe9b9161
Aksel Thomsen (OATH) <oath@novonordisk.com>
2024-07-25 16:11:38 +0200

    docs: documentation of connector objects, and split of methods

--- R/fs.R
+++ R/fs.R
@@ -1,58 +1,95 @@
 #' Connector for file storage
+#'
 #' @description
 #' The connector_fs class is a file storage connector for accessing and manipulating files any file storage solution.
 #' The default implementation includes methods for files stored on local or network drives.
-#' @param name [character] Name of the content to read, write, or remove. Typically the table name.
-#' @param x The object to write
-#' @param file [character] The file to write or save to from the file storage.
-#' @param ... Additional arguments passed to the method.
-#' @param extra_class [character] Extra class to assign to the new connector.
-#' @importFrom R6 R6Class
+
+#' @param name `r rd_connector_params("name")`
+#' @param x `r rd_connector_params("x")`
+#' @param file `r rd_connector_params("file")`
+#' @param ... `r rd_connector_params("...")`
+#' @param extra_class `r rd_connector_params("extra_class")`
+#'
+#' @examples
+#' # Create file storage connector
+#'
+#' cnt <- connector_fs$new(tempdir())
+#'
+#' cnt
+#'
+#' # List content
+#'
+#' cnt$cnt_list_content()
+#'
+#' # Write to the connector
+#'
+#' cnt$cnt_write(iris, "iris.rds")
+#'
+#' # Check it is there
+#'
+#' cnt$cnt_list_content()
+#'
+#' # Read the result back
+#'
+#' cnt$cnt_read("iris.rds") |>
+#'   head(5)
+#'
 #' @export
 connector_fs <- R6::R6Class(
   classname = "connector_fs",
   inherit = connector,
   public = list(
 
-    #' @description Initializes the connector_fs class
-    #' @param path Path to the file storage
-    #' @param access Access type ("rw" by default)
+    #' @description
+    #' Initializes the connector for file storage.
+    #' @param path [character] Path to the file storage
+    #' @param access [character] Access type ("rw" by default).
+    #' Checked using [checkmate::assert_directory_exists].
     initialize = function(path, access = "rw", extra_class = NULL) {
       private$.path <- assert_path(path, access)
       super$initialize(extra_class = extra_class)
     },
 
-    #' @description Read content
-    #' @return The result of the read method
+    #' @description
+    #' Download content from the file storage.
+    #' See also [cnt_download].
+    #' @return [invisible] `file`
     cnt_download = function(name, file = basename(name), ...) {
       self %>%
         cnt_download(name, file, ...)
     },
 
-    #' @description Upload a file
-    #' @param x The object to upload to the file storage
+    #' @description
+    #' Upload a file to the file storage.
+    #' See also [cnt_upload].
+    #' @return [invisible] self
     cnt_upload = function(file, name = basename(file), ...) {
       self %>%
         cnt_upload(file, name, ...)
     },
 
-    #' @description Create a directory
-    #' @param name The name of the directory to create
-    #' @return The directory created
+    #' @description
+    #' Create a directory in the file storage.
+    #' See also [cnt_create_directory].
+    #' @param name [character] The name of the directory to create
+    #' @return [invisible] self
     cnt_create_directory = function(name, ...) {
       self %>%
         cnt_create_directory(name, ...)
     },
 
-    #' @description Remove a directory
-    #' @param name The name of the directory to create
+    #' @description
+    #' Remove a directory from the file storage.
+    #' See also [cnt_remove_directory].
+    #' @param name [character] The name of the directory to remove
+    #' @return [invisible] self
     cnt_remove_directory = function(name, ...) {
       self %>%
         cnt_remove_directory(name, ...)
     }
   ),
   active = list(
-    #' @field path Path to the file storage
+    #' @field path [character] Path to the file storage
     path = function() {
       private$.path
     }
@@ -61,63 +98,6 @@ connector_fs <- R6::R6Class(
     .path = character(0)
   )
 )
-
-#' Additional methods for file storage connectors
-#' @description
-#' These methods are additional S3 methods for  [connector_fs].
-#' @seealso [connector_methods]
-#' @param connector_object A [connector_fs] object to be able to use functions from it
-#' @param ... Additional arguments passed to the methods
-#' @name connector_fs_methods
-NULL
-
-#' Download method for connector object
-#' @rdname connector_fs_methods
-#' @export
-cnt_download <- function(connector_object, ...) {
-  UseMethod("cnt_download")
-}
-
-#' @export
-cnt_download.default <- function(connector_object, ...) {
-  method_error_msg()
-}
-
-#' Upload method for connector object
-#' @rdname connector_fs_methods
-#' @export
-cnt_upload <- function(connector_object, ...) {
-  UseMethod("cnt_upload")
-}
-
-#' @export
-cnt_upload.default <- function(connector_object, ...) {
-  method_error_msg()
-}
-
-#' Create directory method for connector object
-#' @rdname connector_fs_methods
-#' @export
-cnt_create_directory <- function(connector_object, ...) {
-  UseMethod("cnt_create_directory")
-}
-
-#' @export
-cnt_create_directory.default <- function(connector_object, ...) {
-  method_error_msg()
-}
-
-#' Remove directory method for connector object
-#' @rdname connector_fs_methods
-#' @export
-cnt_remove_directory <- function(connector_object, ...) {
-  UseMethod("cnt_remove_directory")
-}
-
-#' @export
-cnt_remove_directory.default <- function(connector_object, ...) {
-  method_error_msg()
-}
 
 #' Validate the path and access
 #' @description
