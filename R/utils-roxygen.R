c6d56a91f37456c0bf47429be18716544614d804
Aksel Thomsen (OATH) <oath@novonordisk.com>
2024-07-29 12:48:52 +0200

    fix: linting

--- R/utils-roxygen.R
+++ R/utils-roxygen.R
@@ -5,12 +5,12 @@ rd_connector_utils <- function(param) {
   x <- c(
     "name" = "[character] Name of the content to read, write, or remove. Typically the table name.",
     "x" = "The object to write to the connection",
-    "file" =  "[character] Path to the file to download to or upload from",
+    "file" = "[character] Path to the file to download to or upload from",
     "..." = "Additional arguments passed to the method for the individual connector.",
     "extra_class" = "[character] Extra class to assign to the new connector.",
     "connector_object" = "[connector] The connector object to use.",
     "inv_self" = "[invisible] self.",
     "inv_connector" = "[invisible] connector_object."
-    )
+  )
   x[[param]]
 }
