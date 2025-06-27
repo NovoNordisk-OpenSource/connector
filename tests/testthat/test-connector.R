747ab3ee4e4f43fd88aff669e19e8a8c7d332913
cgid <cgid@novonordisk.com>
2025-06-27 13:22:44 +0200

    fix: unit tests for cli

--- tests/testthat/test-connector.R
+++ tests/testthat/test-connector.R
@@ -1,32 +1,40 @@
 test_that("can create Connector object", {
-  connector_obj <- connectors(
-    test = ConnectorFS$new(path = tempdir())
-  )
+  temp_dir <- withr::local_tempdir("connector_test")
 
-  expect_s3_class(connector_obj, "connectors")
-
-  expect_snapshot(connector_obj)
-
-  expect_type(print_cnt, "closure")
-
-  #####
-  # Datasources
-  #####
-
-  # errors datasources:
-  expect_error(
-    connectors(
-      datasources = "test"
+  withr::with_tempdir(tmpdir = temp_dir, {
+    connector_obj <- connectors(
+      test = ConnectorFS$new(path = ".")
     )
-  )
 
-  expect_error(datasources(NULL))
+    expect_s3_class(connector_obj, "connectors")
+
+    expect_snapshot(connector_obj)
+
+    expect_type(print_cnt, "closure")
+
+    #####
+    # Datasources
+    #####
+
+    # errors datasources:
+    expect_error(
+      connectors(
+        datasources = "test"
+      )
+    )
+
+    expect_error(datasources(NULL))
+  })
 })
 
 
 cli::test_that_cli("Test connector creation", {
-  connector_obj <- connectors(
-    test = ConnectorFS$new(path = tempdir())
-  )
-  expect_snapshot_out(print(connector_obj$test))
+  temp_dir <- withr::local_tempdir("connector_test")
+
+  withr::with_tempdir(tmpdir = temp_dir, {
+    connector_obj <- connectors(
+      test = ConnectorFS$new(path = ".")
+    )
+    expect_snapshot_out(print(connector_obj$test))
+  })
 })
