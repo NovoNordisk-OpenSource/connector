18ed4584cc344a06539e5f7acbedc3ac96f1c351
akselthomsen <thomsenaksel@gmail.com>
2025-07-21 09:55:19 +0200

    fix: silence col_types prints from readr in tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,7 +2,8 @@
 withr::local_options(
   .new = list(
     connector.verbosity_level = "quiet",
-    whirl.verbosity_level = "quiet"
+    whirl.verbosity_level = "quiet",
+    readr.show_col_types = FALSE
   ),
   .local_envir = teardown_env()
 )
